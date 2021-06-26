9d627ab25b2457a60e5010387373718473c12213
droxy <chiomaotu22@gmail.com>
2021-06-26 10:58:59 +0100

    changes

--- index.php
+++ index.php
@@ -47,48 +47,13 @@
 
         //Login Validation
 
-    
-
-        //1. FORM DATA + FETCH
+         //1. FORM DATA + FETCH
 
         const formData = new FormData(this);
-        
-        // data.append("email", em.value);
-        // data.append("password", pw.value);
 
-         fetch("receive.php", {
+         fetch("login.php", {
           method: "post",
-          body: formData,
-          headers: {
-            "Content-Type": "application/json",
-          },
-        })
-          .then(function (response) {
-            return response.text();
-          })
-          .then(function (text) {
-            return (text);
-            console.log(text);
-          })
-          .catch(function (error) {
-            console.error(error);
-          });
-        
-
-
-        //2. USER OBJECT + FETCH
-       /* 
-        const user = {
-          email: em.value,
-          password: pw.value,
-        };
-        
-        fetch("receive.php", {
-          method: "post",
-          body: JSON.stringify(user),
-          headers: {
-            "Content-Type": "application/json",
-          },
+          body: formData
         })
           .then(function (response) {
             return response.text();
@@ -98,23 +63,7 @@
           })
           .catch(function (error) {
             console.error(error);
-          });
-          
-          */
-
-        //3. FORM DATA + AJAX
-        /*
-        const data = new FormData();
-        
-        data.append("email", em.value);
-        data.append("password", pw.value);
-        
-        const xhr = new XMLHttpRequest();
-        
-        xhr.open("POST", "receive.php", true);
-        
-        xhr.send(data);
-        */
+          });    
       });
     </script>
   </body>
